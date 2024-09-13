b5bf901cf5e38d5dac31487967718c777781ea40
FreshinS <134167394+FreshinS@users.noreply.github.com>
2024-09-13 11:53:01 +0000

    several fixes + sethotel

--- src/setters/set-hotel.js
+++ src/setters/set-hotel.js
@@ -1,27 +1,35 @@
 import updater from '../file-updater.js';
 import getId from '../getters/get-id.js';
 import readlineSync from 'readline-sync';
+import reader from '../file-reader.js';
+import setRegion from './set-region.js';
 
-const newHotel = readlineSync.question('New hotel: ').toLowerCase().trim();
-const id = getId('data/hotels.csv');
-const regionToAdd = readlineSync.question('Region: ').toLowerCase().trim();
-const regions = reader('data/regions.csv');
-const filtered = regions.filter((string) => {
-    const [id, name] = string.split(';');
-    return name === regionToAdd ? true : false;
-});
-console.log(filtered);
+const setHotel = () => {
+    const newHotel = readlineSync.question('New hotel: ').toLowerCase().trim();
+    const id = getId('data/hotels.csv');
+    const regionToAdd = readlineSync.question('Region: ').toLowerCase().trim();
+    const regions = reader('data/regions.csv');
+    const filtered = regions.filter((string) => {
+        const [, name] = string.split(';');
+        return name === regionToAdd ? true : false;
+    });
+    console.log(filtered);
 
-let regionId;
-if (filtered !== 0) {
-    regionId = filtered.at(0).split(';').at(0);
-} else {
-    console.log(`Region ${regionToAdd} does not exist.`);
-    const markToAdd = readlineSync.question('Would you like to add region? [y/n]')
-    .toLowerCase().trim();
-    if (markToAdd === 'y') {
-        regionId = setRegion(regionToAdd);
+    let regionId;
+    if (filtered.length !== 0) {
+        regionId = filtered.at(0).split(';').at(0);
     } else {
-        return false;
+        console.log(`Region ${regionToAdd} does not exist.`);
+        const markToAdd = readlineSync.question('Would you like to add region? [y/n]')
+        .toLowerCase().trim();
+        if (markToAdd === 'y') {
+            regionId = setRegion(regionToAdd);
+        } else {
+            return false;
+        }
     }
-} 
+    updater('data/hotels.csv', `\n${id};${newHotel};${regionId}`)
+    return true; 
+};
+
+setHotel();
--- src/setters/set-region.js
+++ src/setters/set-region.js
@@ -2,12 +2,14 @@ import updater from '../file-updater.js';
 import getId from '../getters/get-id.js';
 import readlineSync from "readline-sync";
 
-const setRegion = (fPath) => {
-    const newRegion = readlineSync.question('New region: ').toLowerCase().trim();
-    const id = getId(fPath);
-
-    updater('data/regions.csv', `\n${id}|${newRegion}`);
-    return true;
+const setRegion = (name='') => {
+    let newRegion
+    if (name === '') {
+        newRegion = readlineSync.question('New region: ').toLowerCase().trim();
+    } else newRegion = name;
+    const id = getId('data/regions.csv');
+    updater('data/regions.csv', `\n${id};${newRegion}`);
+    return id;
 }
 
-setRegion('data/regions.csv');
+export default setRegion;
